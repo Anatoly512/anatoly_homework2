568d4f881e008c775bf7b787e2156316eb4fc0e2
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-09 00:03:04 +0200

    New structure of program - with classes

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.print("\nPlease, enter your NAME :  ");
         String name = inputData.stringInput();
 
-        System.out.println("\nYour NAME is :  " + name);
+
 
     }
 
