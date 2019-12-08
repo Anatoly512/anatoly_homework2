61f061e37ab35778f26f54296efba8aaa9900d04
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-08 23:33:17 +0200

    added input data modules

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+
+    public static String stringInput () {
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        return name;
+    }
+
+
+    public static int numberInput () {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        return number;
+    }
+
+
 }
+
+//  Домашняя работа 02
