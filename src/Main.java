e573ee46e02aa5b7af77606809668e5e31e83149
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-09 15:37:31 +0200

    1 and 2 text output forms done

--- src/Main.java
+++ src/Main.java
@@ -67,7 +67,7 @@ public class Main {
 
     public static void output3 (String name, String city, int age, String hobby) {
         // other variant
-        System.out.println("\nVariant:  3 ");
+        System.out.println("\n:  ");
     }
 
 
