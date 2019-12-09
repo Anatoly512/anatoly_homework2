121e43645da538b0ab10874c65385dc3f82a92fd
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-09 02:21:23 +0200

    working structure of program

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         String name = inputData.stringInput();
 
         System.out.print("\nWhat city do you LIVE in :  ");
-        String currentCity = inputData.stringInput();
+        String city = inputData.stringInput();
 
         System.out.print("\nWhat's your AGE :  ");
         int age = inputData.numberInput();
@@ -24,10 +24,44 @@ public class Main {
         System.out.print("\nWhat is your hobby :  ");
         String hobby = inputData.stringInput();
 
-        System.out.print("\n " + name + "\n " + currentCity + "\n " + age + "\n " + hobby);
+        System.out.println("\nThank you!");
+        System.out.println("\nPlease, choose variants:  1, 2 or 3  : ");
+        choice(name, city, age, hobby);
+    }
 
+
+    public static void choice (String name, String city, int age, String hobby) {
+        int number;
+        number = inputData.numberInput();
+
+           if (number == 1) {
+               output1(name, city, age, hobby);
+           }
+           if (number == 2) {
+               output2(name, city, age, hobby);
+           }
+           if (number == 3) {
+               output3(name, city, age, hobby);
+           } else {
+               System.out.println("\nPlease, try again (possible variants: 1, 2 or 3) : ");
+               choice(name, city, age, hobby);
+
+           }
 
     }
+
+    public static void output1 (String name, String city, int age, String hobby) {
+        System.out.println("\nVariant:  1 ");
+    }
+
+    public static void output2 (String name, String city, int age, String hobby) {
+        System.out.println("\nVariant:  2 ");
+    }
+
+    public static void output3 (String name, String city, int age, String hobby) {
+        System.out.println("\nVariant:  3 ");
+    }
+
 
 
 }
